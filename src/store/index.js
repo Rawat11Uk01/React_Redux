fdecdfebf2577376dc59346118902527c68e258b
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-03-18 21:17:38 +0530

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,39 @@
-import {createStore} from 'redux';
-const counterReducer = (state= {counter:0},action)=>{
-  if(action.type==="INCREAMENT"){
-    return{
-        counter: state.counter + 1
-    }
-    
-}
+import { createStore } from 'redux';
 
-if(action.type==="DECREAMENT"){
-    return{
-        counter: state.counter - 1
-    }
+const initialState = { counter: 0, showCounter: true };
 
-}
+const counterReducer = (state = initialState, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter
+    };
+  }
 
-return{state} 
-}
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter
+    };
+  }
+
+  if (action.type === 'toggle') {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter
+    };
+  }
+
+  return state;
+};
 
 const store = createStore(counterReducer);
+
 export default store;
